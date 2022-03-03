1ac02b13a10f7084ec9e7aff73d7b37199fade4f
WookeyKim95 <bluebear9508@naver.com>
2022-03-04 08:00:16 +0900

    adding file for reference

---
+++ dummy_open3d_plan.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  4 07:56:51 2022
+
+@author: blueb
+"""
+
+"""
+import numpy as np
+import open3d as o3d : open3d 모듈 import하기
+
+input_path="your_path_to_file/"
+output_path="your_path_to_output_folder/"
+dataname="sample.xyz"
+point_cloud= np.loadtxt(input_path+dataname,skiprows=1) : 파일 불러오기
+
+o3d.visualization.draw_geometries([pcd]) : 메쉬 시각화 하기
+
+
+출처: https://engineer-mole.tistory.com/248 [매일 꾸준히, 더 깊이]
+"""
