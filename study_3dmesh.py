54a2b9d9f4e2aa9e9a624830db4b8e0369cfeb90
wookeykim95 <bluebear9508@naver.com>
2022-03-18 06:36:16 +0900

    rename and added contents

--- study_3dmesh.py
+++ study_3dmesh.py
@@ -27,6 +27,23 @@ pcd.normals = o3d.utility.Vector3dVector(point_cloud[:,6:9])
 # 포인트 클라우드 시각화
 o3d.visualization.draw_geometries([pcd])
 
+#bpa 방법
+
+# 반경매개변수계산
+distances = pcd.compute_nearest_neighbor_distance()
+avg_dist = np.mean(distances)
+radius = 3 * avg_dist
+
+bpa_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pcd,o3d.utility.DoubleVector([radius, radius * 2]))
+
+# 여기의 mesh 자리에다가 파일을 입력할 필요가 있어보임. 오류발생.
+dec_mesh = mesh.simplify_quadric_decimation(100000)
+
+dec_mesh.remove_degenerate_triangles()
+dec_mesh.remove_duplicated_triangles()
+dec_mesh.remove_duplicated_vertices()
+dec_mesh.remove_non_manifold_edges()
+
 # 포아송 REstruction
 poisson_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=8, width=0, scale=1.1, linear_fit=False)[0]
 
